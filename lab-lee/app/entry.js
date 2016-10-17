32b39af7e4118718970ac0ff5878bf04aa28d934
Lee Broxson <broxsonl@gmail.com>
2016-10-17 16:38:22 -0700

    Bonus 2 done.

--- lab-lee/app/entry.js
+++ lab-lee/app/entry.js
@@ -16,20 +16,33 @@ const cowsayApp = angular.module('cowsayApp', []);
 cowsayApp.controller('CowsayController', [ '$log', '$scope', CowsayController]);
 
 function CowsayController($log, $scope) {
+
   $log.debug('init CowsayController');
   let cowsayCtrl = $scope.cowsayCtrl = {};
   cowsayCtrl.title = 'I am a Cow! I say stuff!';
   cowsayCtrl.savedTitle = 'I am a cow who repeats when the button is clicked!';
 
+  cowsayCtrl.array = [];
+
   cowsayCtrl.updateCow = function(input) {
     $log.debug('cowsayCtrl.updateCow()');
     return '\n' + cowsay.say({text: input || 'Give me something to say, human!'});
+  };
+  
+  cowsayCtrl.updateThinkCow = function(input) {
+    $log.debug('cowsayCtrl.updateCow()');
+    return '\n' + cowsay.think({text: input || 'Give me something to say, human!'});
   };
 
   cowsayCtrl.helloClick = function(input) {
     $log.debug('cowsayCtrl.helloClick()');
     $log.debug(input);
-    return '\n' + cowsay.say({text: input || 'I\'m ready to repeat!'});
+    cowsayCtrl.array.push(input);
+    cowsayCtrl.speak = cowsayCtrl.updateThinkCow(cowsayCtrl.updateCow(input));
   };
 
+  cowsayCtrl.backClick = function() {
+    $log.debug('cowsayCtrl.backClick()');
+    cowsayCtrl.speak = cowsayCtrl.updateCow(cowsayCtrl.updateThinkCow(cowsayCtrl.array.pop()));
+  };
 }
