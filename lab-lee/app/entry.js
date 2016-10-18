7516a2aa239785b24d49d1b0303e7050b780acd3
Lee Broxson <broxsonl@gmail.com>
2016-10-18 12:27:32 -0700

    Followed along with code during lecture.

--- lab-lee/app/entry.js
+++ lab-lee/app/entry.js
@@ -15,34 +15,54 @@ const cowsayApp = angular.module('cowsayApp', []);
 // angular constructs
 cowsayApp.controller('CowsayController', [ '$log', '$scope', CowsayController]);
 
-function CowsayController($log, $scope) {
+function CowsayController($log) {
 
   $log.debug('init CowsayController');
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  cowsayCtrl.title = 'I am a Cow! I say stuff!';
-  cowsayCtrl.savedTitle = 'I am a cow who repeats when the button is clicked!';
+  this.title = 'I am a Cow! I say stuff!';
+  this.savedTitle = 'I am a cow who repeats when the button is clicked!';
 
-  cowsayCtrl.array = [];
+  this.array = [];
 
-  cowsayCtrl.updateCow = function(input) {
-    $log.debug('cowsayCtrl.updateCow()');
+  cowsay.list((err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.currentCow = this.cowfiles[0];
+    console.log(cowfiles);
+  });
+
+  this.updateCow = function(input) {
+    $log.debug('this.updateCow()');
     return '\n' + cowsay.say({text: input || 'Give me something to say, human!'});
   };
-  
-  cowsayCtrl.updateThinkCow = function(input) {
-    $log.debug('cowsayCtrl.updateCow()');
-    return '\n' + cowsay.think({text: input || 'Give me something to say, human!'});
+
+  this.updateThinkCow = function(input) {
+    $log.debug('this.updateCow()');
+    return '\n' + cowsay.think({text: input || 'Give me something to say, human!', f: this.currentCow});
   };
 
-  cowsayCtrl.helloClick = function(input) {
-    $log.debug('cowsayCtrl.helloClick()');
-    $log.debug(input);
-    cowsayCtrl.array.push(input);
-    cowsayCtrl.speak = cowsayCtrl.updateThinkCow(cowsayCtrl.updateCow(input));
+  this.talk = function(input) {
+    $log.debug('this.updateCow()');
+    this.speak = this.updateCow(input);
+    this.array.push(this.talk);
   };
 
-  cowsayCtrl.backClick = function() {
-    $log.debug('cowsayCtrl.backClick()');
-    cowsayCtrl.speak = cowsayCtrl.updateCow(cowsayCtrl.updateThinkCow(cowsayCtrl.array.pop()));
+  this.undo = function() {
+    this.talk = this.history.pop() || '';
   };
+}
+
+cowsayApp.controller('NavController', ['$log', NavController]);
+
+function NavController($log) {
+  $log.debug('init NavController');
+  // property on the NavController called this.routes hat has 3 routes on it.
+  this.routes = [
+    {
+      name: 'Home',
+      url: '/home',
+    },
+    {
+      name: 'About',
+      url: '/about',
+    },
+  ];
 }
--- lab-lee/webpack.config.js
+++ lab-lee/webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: `${__dirname}/app/entry.js`,
@@ -12,12 +13,23 @@ module.exports = {
     new HTMLPlugin({
       template: `${__dirname}/app/html/index.html`,
     }),
+    new ExtractTextPlugin('bundle.css'),
   ],
   module: {
     loaders: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
         test: /\.scss$/,
-        loader: 'style!css!sass!',
+        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'),
+      },
+      {
+        test: /\.(eot|wof|ttf|svg).*/,
+        //if the file is less than 10kb, put them into the fonts folder, otherwise
+        loader: 'url?limit=10000&path=fonts/[hash].[ext]',
       },
     ],
   },
