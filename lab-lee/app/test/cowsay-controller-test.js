41171bd6ce4b03c026a4618b1d22842c93ac4fcd
Lee Broxson <broxsonl@gmail.com>
2016-10-19 13:45:00 -0700

    Got initial setup done. Need to write tests.

---
+++ lab-lee/app/test/cowsay-controller-test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('./lib/test-setup.js');
+
+const cowsay = require('cowsay-browser');
+const angular = require('angular');
+
+// describe('testing cowsayCtrl', function() {
+//
+//   beforeEach()
+// })
+
+describe('testing karma success', function() {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
